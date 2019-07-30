964a3f00654be2d846dd87a6ef10c67c35d599c7
Esther Baek <esther.baek5@gmail.com>
2019-07-29 20:21:53 -0400

    add global user methods for controller and views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,25 @@
 class ApplicationController < ActionController::Base
+    helper_method :current_user, :logged_in?
+
+    def current_user
+        @current_user ||= User.find_by(session_token: session[:session_token])
+    end
+
+    def ensure_logged_in
+        redirect_to root_url
+    end
+
+    def logged_in?
+        !!current_user
+    end
+
+    def login!(user)
+        session[:session_token] = user.reset_session_token!
+    end
+
+    def logout!
+        current_user.reset_session_token!
+        @current_user = nil
+        session[:session_token] = nil
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   root to: 'static_pages#root'
 
   namespace :api, defaults: {format: :json} do
-    resources :users, only: [:index, :show]
+    resources :users, only: [:create, :show]
+    resource :session, only: [:create, :destroy]
   end
-
 
 end

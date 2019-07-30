324e70356c75e4af8af75a567f6b44d4d34c3449
Esther Baek <esther.baek5@gmail.com>
2019-07-30 10:41:32 -0400

    add default portfolio value and fixed find by credentials user model

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -2,7 +2,6 @@ class Api::UsersController < ApplicationController
 
     def create
         @user = User.new(user_params)
-
         if @user.save
             login!(@user)
             render "api/users/show"
@@ -13,6 +12,6 @@ class Api::UsersController < ApplicationController
 
     private
     def user_params
-        params.require(:user).permit(:email, :password)
+        params.require(:user).permit(:email, :password, :first_name, :last_name)
     end
 end

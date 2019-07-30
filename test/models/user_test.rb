1eeeaa297dfba8a99b0ba09b5b0c798220ef08ba
Esther Baek <esther.baek5@gmail.com>
2019-07-29 20:04:42 -0400

    user model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  first_name      :string           not null
+#  last_name       :string           not null
+#  email           :string           not null
+#  password_digest :string           not null
+#  portfolio_value :float            not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase

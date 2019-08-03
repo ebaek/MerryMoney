af86d43bf37c109c8b377a9ecf1ab20457b207f8
Esther Baek <esther.baek5@gmail.com>
2019-08-03 08:27:31 -0400

    working historical line chart

--- frontend/components/company/chart.jsx
+++ frontend/components/company/chart.jsx
@@ -6,26 +6,25 @@ class Chart extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = {low: {}, high: {}};
+        this.state = {prices: []};
     }
 
     componentDidMount() {
         this.ticker = this.props.ticker;
-        this.fiveYr = this.props.fetchCompanyHistoricPrices(this.ticker, "5y", "5");
+        this.props.fetchCompanyHistoricPrices(this.ticker, "5y", "5").then( (res) => this.setState(res));
     }
 
     render() {
-        const data = this.fiveYr
-
-        
+        const prices = this.state.prices;
 
         return (
             <div className="chart-container">
-                <LineChart width={600} height={300} data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
-                    <Line type="monotone" dataKey="400" stroke="#8884d8" />
+                <LineChart width={600} height={300} data={prices} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
+                    <Line type="monotone" dataKey="uClose" stroke="#8884d8" />
                     <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-                    <XAxis dataKey="close" />
+                    <XAxis dataKey="date" />
                     <YAxis />
+                    <Tooltip />
                 </LineChart>
             </div>
         );

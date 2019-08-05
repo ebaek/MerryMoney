9cba443628a9132987db684b8e24bee9a83991c3
Esther Baek <esther.baek5@gmail.com>
2019-08-05 08:43:47 -0400

    add diff from current time on news

--- frontend/components/company/news.jsx
+++ frontend/components/company/news.jsx
@@ -15,6 +15,42 @@ class News extends React.Component {
             .then(this.props.fetchNews(ticker).then(res => this.setState(res)));
     }
 
+    publicationDate(date) {
+        if(date !== undefined) {
+            let newDate = new Date(date);
+            let now = new Date(Date.now());
+            
+            if(now.getFullYear() === newDate.getFullYear()) {
+                if(now.getMonth() === newDate.getMonth()) {
+                    if(now.getDate() === newDate.getDate()) {
+                        if(now.getHours() === newDate.getHours()) {
+                            if(now.getMinutes() - newDate.getMinutes()) {
+                                const diff = now.getSeconds() - newDate.getSeconds();
+                                return `${diff}s`
+                            } else {
+                                const diff = now.getMinutes() - newDate.getMinutes();
+                                return `${diff}m`
+                            }
+                        } else {
+                            const diff = now.getHours() - newDate.getHours();
+                            return `${diff}h`
+                        }
+                    } else {
+                        const diff = now.getDate() - newDate.getDate();
+                        return `${diff}d`
+                    }
+                } else {
+                    const diff = now.getMonth() - newDate.getMonth();
+                    return `${diff}m`
+                }
+            } else {
+                const diff = now.getFullYear() - newDate.getFullYear();
+                return `${diff}y`
+            }
+        }
+
+    }
+
     formatArticles() {
         let articles = Object.assign([], this.state.news.articles);
         
@@ -24,6 +60,7 @@ class News extends React.Component {
                     <div className="news-box">
                         <div className="news-description">
                             <p className="article-source">{article.source.name}</p>
+                            <p className="publication-date">{this.publicationDate(article.publishedAt)}</p>
                             <p className="article-title">{article.title}</p>
                         </div>
 
